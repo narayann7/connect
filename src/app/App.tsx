6888114227efe9d987bb07f63b61e930bb86b1da
narayann7 <laxminarayanreddy432@gmail.com>
2026-04-11 23:55:03 +0530

    update : background

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import AnimatedBackground from "./components/AnimatedBackground";
 import Core from "./components/Core";
 import NotFound from "./components/NotFound";
-import SilkBackground from "./components/SilkBackground";
 
 function App() {
   return (
     <div className="relative min-h-screen w-full bg-black overflow-hidden">
-      {/* Silk shader background */}
-      <SilkBackground
-        className="absolute inset-0"
-        speed={15}
-        scale={1}
-        color="#0f2927"
-        opacity={0.3}
-      />
+        {/* Animated gradient background */}
+      <AnimatedBackground />
 
       {/* Main content */}
       <div className="flex items-center justify-center flex-col relative z-10">
